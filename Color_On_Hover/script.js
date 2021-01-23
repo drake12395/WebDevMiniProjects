1550ebb25063a4ed51ffa8a3e1dddc25159c84e6
Drake Schmidt <drake12395@gmail.com>
2021-01-23 07:25:11 -0500

    color palette options

--- Color_On_Hover/script.js
+++ Color_On_Hover/script.js
@@ -1,6 +1,9 @@
 const container = document.getElementById('container');
-const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
-const squares = 600;
+let allColors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
+let greyScale = ['#E0E0E0', '#B0B0B0', '#888888', '#F8F8F8', '#202020'];
+let blueScale = ['#B0E2FF', '#3299CC', '#236B8E', '#05EDFF', '#2E37FE'];
+let redScale = ['#ff0000', '#ff4c4c', '#ff7f7f', '#ffb2b2', '#ffcccc'];
+const squares = 400;
 
 for (let i = 0; i < squares; i++) {
   const square = document.createElement('div');
@@ -13,7 +16,7 @@ for (let i = 0; i < squares; i++) {
 }
 
 function setColor(el) {
-  const color = getRandomColor();
+  const color = getColor();
   el.style.background = color;
   el.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
@@ -23,6 +26,22 @@ function removeColor(el) {
   el.style.boxShadow = '0 0 2px #000';
 }
 
-function getRandomColor() {
-  return colors[Math.floor(Math.random() * colors.length)];
+function getColor() {
+  document.getElementById('grey').addEventListener('click', function () {
+    allColors = greyScale;
+  });
+
+  document.getElementById('blue').addEventListener('click', function () {
+    allColors = blueScale;
+  });
+
+  document.getElementById('red').addEventListener('click', function () {
+    allColors = redScale;
+  });
+
+  document.getElementById('allColors').addEventListener('click', function () {
+    allColors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
+  });
+
+  return allColors[Math.floor(Math.random() * allColors.length)];
 }
